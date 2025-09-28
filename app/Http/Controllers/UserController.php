4ef178baa25dcf4459be68753e628df9a5e442c3
Rajesh Mondal <rajesh01749@gmail.com>
2025-09-29 00:07:56 +0600

    create roles and permissions with assignments to users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
@@ -23,6 +25,52 @@ class UserController extends Controller {
         return response()->json( ['message' => 'User Created Successfully'] );
     }
 
+    public function createRole( Request $request ) {
+        $data = $request->validate( [
+            'name'         => 'required',
+            'display_name' => 'required',
+            'description'  => 'required',
+        ] );
+
+        Role::create( $data );
+        return response()->json( ['message' => 'Role Created Successfully'] );
+    }
+
+    public function createPermission( Request $request ) {
+        $data = $request->validate( [
+            'name'         => 'required',
+            'display_name' => 'required',
+            'description'  => 'required',
+        ] );
+
+        Permission::create( $data );
+        return response()->json( ['message' => 'Permission Created Successfully'] );
+    }
+
+    public function assignPermissionToRole( Request $request ) {
+        $data = $request->validate( [
+            'role'       => 'required',
+            'permission' => 'required',
+        ] );
+
+        $role = Role::where( 'name', $data['role'] )->first();
+        $role->givePermission( $data['permission'] );
+
+        return response()->json( ['message' => 'Assign Pemission to Role Successfully'] );
+    }
+
+    public function assignRoleToUser( Request $request ) {
+        $data = $request->validate( [
+            'user_id' => 'required',
+            'role'    => 'required',
+        ] );
+
+        $user = User::findOrFail( $data['user_id'] );
+        $user->addRole( $data['role'] );
+
+        return response()->json( ['message' => 'Assign Role to User Successfully'] );
+    }
+
     public function loginUser( Request $request ) {
         $data = $request->validate( [
             'email'    => 'required',
@@ -31,7 +79,7 @@ class UserController extends Controller {
 
         if ( Auth::attempt( ['email' => $data['email'], 'password' => $data['password']] ) ) {
             return response()->json( ['message' => 'User Login Successfully'] );
-        } else{
+        } else {
             return response()->json( ['message' => 'Invalid Credencials'] );
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get( '/', function () {return view( 'welcome' );} );
 Route::post( '/createUser', [UserController::class, 'createUser'] );
 Route::post( '/loginUser', [UserController::class, 'loginUser'] );
+Route::post( '/createRole', [UserController::class, 'createRole'] );
+Route::post( '/createPermission', [UserController::class, 'createPermission'] );
+Route::post( '/assignPermissionToRole', [UserController::class, 'assignPermissionToRole'] );
+Route::post( '/assignRoleToUser', [UserController::class, 'assignRoleToUser'] );
